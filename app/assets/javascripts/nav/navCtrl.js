4e98b92372a356233967188b678e326442fadc79
Han Lee <ralphgoodwin@Ralphs-MacBook-Air.local>
2017-02-20 14:37:32 -0500

    build navCtrl

---
+++ app/assets/javascripts/nav/navCtrl.js
@@ -0,0 +1,23 @@
+angular.module('flapperNews')
+  .controller('NavCtrl', ['$scope', 'Auth',
+    function($scope, Auth) {
+      $scope.signedIn = Auth.isAuthenticated;
+      $scope.logout = Auth.logout;
+
+      Auth.currentUser()
+        .then(user => $scope.user = user)
+      });
+
+      $scope.$on('devise:new-registration', function(e, user) {
+        $scope.user = user;
+      });
+
+      $scope.$on('devise:login', function(e, user) {
+        $scope.user = user;
+      });
+
+      $scope.$on('devise:logout', function(e, user) {
+        $scope.user = {};
+      })
+
+    }]);
